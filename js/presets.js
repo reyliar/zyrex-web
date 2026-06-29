0cc3f2ff42a0558e68b6b37df067e290b8074372
reyli <129988691+reyliar@users.noreply.github.com>
2026-06-29 03:54:11 +0300

    Uploader Discord avatar, countdown timer, remove test product, cleanup Free label

--- js/presets.js
+++ js/presets.js
@@ -73,8 +73,7 @@ function renderPresets(items) {
             '<div class="rc-footer">' +
             '<div class="rc-meta">' +
             '<div class="rava-fb" style="overflow:hidden;display:flex;align-items:center;justify-content:center">' + avatarHtml + '</div>' +
-            '<span class="rname">' + nickname + '</span>' +
-            '<span class="rdate">Free</span></div>' +
+            '<span class="rname">' + nickname + '</span></div>' +
             '<div class="rc-actions">' +
             '<span><i class="fas fa-download"></i> 0</span>' +
             '<span><i class="fas fa-heart"></i> 0</span>' +
